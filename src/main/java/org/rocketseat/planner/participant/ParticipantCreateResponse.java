5ea3e36ba3143229ea2770471a9972f81f80afdf
Leandro Amaral <lramaral02m@gmail.com>
2024-07-09 22:43:41 -0300

    Subindo aula 02 - Participantes

---
+++ src/main/java/org/rocketseat/planner/participant/ParticipantCreateResponse.java
@@ -0,0 +1,6 @@
+package org.rocketseat.planner.participant;
+
+import java.util.UUID;
+
+public record ParticipantCreateResponse(UUID id) {
+}
---
+++ src/main/java/org/rocketseat/planner/participant/ParticipantRequestPayload.java
@@ -0,0 +1,4 @@
+package org.rocketseat.planner.participant;
+
+public record ParticipantRequestPayload(String name, String email) {
+}
